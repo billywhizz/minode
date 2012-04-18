e23bca8307cc94dd0a8504ad8c37df15b4858e4e
Andrew Johnston <apjohnsto@gmail.com>
2012-04-18 07:54:54 +0100

    changed repo to be just the library. demos are available at git@icms:minsock-demo.git

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.http = require("./lib/http");
+exports.utils = require("./lib/utils");
+exports.websock = require("./lib/websock");
+exports.mime = require("./lib/mime");
+exports.minsock = require("./lib/minsock");
+exports.filecache = require("./lib/filecache");
+exports.forkit = require("./lib/forkit");
+exports.ftp = require("./lib/ftp.js");
